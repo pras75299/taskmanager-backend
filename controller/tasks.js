21361db27001faacb91c81f7f145e7a0ff258b3b
pras75299 <singh.prashantking@gmail.com>
2023-11-26 18:59:30 +0530

    all api basic structure complete

--- controller/tasks.js
+++ controller/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
   res.send("all items from the file");
 };
 
+const createTasks = (req, res) => {
+  res.send("create task");
+};
+
+const getTask = (req, res) => {
+  res.send("get single task from the file");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task on file");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task from the file");
+};
+
 module.exports = {
   getAllTasks,
+  createTasks,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getAllTasks } = require("../controller/tasks");
+const {
+  getAllTasks,
+  createTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/tasks");
 
 router.get("/", getAllTasks);
+router.post("/", createTasks);
+router.get("/:id", getTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
