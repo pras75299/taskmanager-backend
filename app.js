2439b6d62630453092c042228ff6ef704002b0aa
pras75299 <singh.prashantking@gmail.com>
2023-11-26 18:52:00 +0530

    basic setup for api complete

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const tasks = require("./routes/tasks");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/tasks", tasks);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Everything is working fine ${PORT}`);
+});
